a7625d729aa8bbf68e5f99990cfbeb879c8242ba
ElcioCestari <elciotaira@gmail.com>
2023-05-05 20:28:46 -0300

    feat: adicionado service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TelaLoginComponentComponent } from './components/tela-login-component/tela-login-component.component';
+import { TelaLoginComponentComponent } from './components/telas/tela-login-component/tela-login-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TelaLoginComponentComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TelaLoginComponentComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/telas/tela-login-component/tela-login-component.component.ts
+++ src/app/components/telas/tela-login-component/tela-login-component.component.ts
@@ -1,10 +1,19 @@
+import { UserService } from './../../../services/user.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tela-login-component',
   templateUrl: './tela-login-component.component.html',
-  styleUrls: ['./tela-login-component.component.css']
+  styleUrls: ['./tela-login-component.component.css'],
 })
 export class TelaLoginComponentComponent {
+  private service: UserService;
 
+  constructor(service: UserService) {
+    this.service = service;
+  }
+
+  login() {
+    this.service.login();
+  }
 }
