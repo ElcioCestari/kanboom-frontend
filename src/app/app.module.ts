c3adc8489329af52b465d5fb277baba1ca38b2d9
Tobias Ramos <tobiasramossilva@gmail.com>
2023-05-08 20:55:08 -0300

    Acessando  o json pelo service e salvando o email e senha do usuário no sessionStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TelaLoginComponentComponent } from './components/telas/tela-login-component/tela-login-component.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, TelaLoginComponentComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
