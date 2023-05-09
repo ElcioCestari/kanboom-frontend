344e9b6ee0e963627dc5a9672a04b9b1e84d42a9
Tobias Ramos <tobiasramossilva@gmail.com>
2023-05-09 14:10:54 -0300

    Criando o componete da tela de criar cartões

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { TelaLoginComponentComponent } from './components/telas/tela-login-component/tela-login-component.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { PageCreateNewBoardComponent } from './components/telas/page-create-new-board-component/page-create-new-board.component';
 
 @NgModule({
-  declarations: [AppComponent, TelaLoginComponentComponent],
+  declarations: [AppComponent, TelaLoginComponentComponent, PageCreateNewBoardComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
