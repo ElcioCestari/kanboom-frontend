cb7f79cc40b3e3ac1ae150cfecb1fd141e7dd7e3
ElcioCestari <elciotaira@gmail.com>
2023-05-11 22:30:42 -0300

    fix problems

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,36 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-<<<<<<< HEAD
 import { Usuario } from '../../model/usuario';
 import { UsuarioCreate } from 'src/model/usuario_create';
-
-
-=======
-import { Usuario } from '../components/telas/tela-login-component/usuario';
 import { Router } from '@angular/router';
->>>>>>> main
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient, private router: Router) {
-  }
+  constructor(private http: HttpClient, private router: Router) {}
 
   login(usuario: Usuario) {
-    let url = "http://localhost:3000/users/login";
-    this.http.post(url, usuario).toPromise().then((data: any) => {
-
-      if (usuario.username === data[0].username && usuario.password === data[0].password) {
-        sessionStorage.setItem("usuario", usuario.username)
-        this.router.navigate(['/page-onboarding']);
-      } else {
-        alert("usuário não cadastrado")
-      }
-    }).catch(error => {
-      alert(error?.error?.message)
-    })
-
+    let url = 'http://localhost:3000/users/login';
+    this.http
+      .post(url, usuario)
+      .toPromise()
+      .then((data: any) => {
+        if (
+          usuario.username === data[0].username &&
+          usuario.password === data[0].password
+        ) {
+          sessionStorage.setItem('usuario', usuario.username);
+          this.router.navigate(['/page-onboarding']);
+        } else {
+          alert('usuário não cadastrado');
+        }
+      })
+      .catch((error) => {
+        alert(error?.error?.message);
+      });
 
     //chamar o backend para efetuar o login
     //salvar o usuário no sessionStorage
@@ -38,36 +36,35 @@ export class UserService {
 
   async save(usuario: UsuarioCreate) {
     // valida se os campos foram preenchidos
-    if (!usuario.email || !usuario.nome || !usuario.senha || !usuario.confirma) {
-      alert("Preencha todos os campos.");
+    if (
+      !usuario.email ||
+      !usuario.nome ||
+      !usuario.senha ||
+      !usuario.confirma
+    ) {
+      alert('Preencha todos os campos.');
       return;
     }
 
     // Verificar se as senhas são iguais
     if (usuario.senha !== usuario.confirma) {
-      alert("As senhas precisam ser iguais.");
+      alert('As senhas precisam ser iguais.');
       return;
     }
 
     // Verificar se as senhas são iguais
     if (usuario.senha !== usuario.confirma) {
-      alert("As senhas precisam ser iguais.");
+      alert('As senhas precisam ser iguais.');
       return;
     }
 
     this.salvaUsuario(usuario).subscribe(() => {
-      alert("Usuário cadastrado com sucesso!");
+      alert('Usuário cadastrado com sucesso!');
     });
-
-    
-    
   }
 
-  
-
   private salvaUsuario(usuario: UsuarioCreate) {
-    let url = "http://localhost:3000/users";
+    let url = 'http://localhost:3000/users';
     return this.http.post(url, usuario);
   }
 }
-
