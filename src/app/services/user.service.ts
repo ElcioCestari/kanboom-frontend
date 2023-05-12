fc0044cedcd8029bc8cf3a250861f35f720d80e5
Tobias Ramos <tobiasramossilva@gmail.com>
2023-05-11 21:04:07 -0300

    Fazendo a mudaça de telas e tratando o erro do login

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,24 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Usuario } from '../components/telas/tela-login-component/usuario';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   login(usuario: Usuario) {
-    let url = "http://localhost:3000/users";
-    this.http.get(url, {
-    }).toPromise().then((data: any) => {
+    let url = "http://localhost:3000/users/login";
+    this.http.post(url, usuario).toPromise().then((data: any) => {
 
       if (usuario.username === data[0].username && usuario.password === data[0].password) {
         sessionStorage.setItem("usuario", usuario.username)
+        this.router.navigate(['/page-onboarding']);
       } else {
         alert("usuário não cadastrado")
       }
+    }).catch(error => {
+      alert(error?.error?.message)
     })
 
 
